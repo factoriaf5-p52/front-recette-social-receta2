1b2bf5f0187566775b2497be02ed6c86abeb5631
Freddy Vegas <vegasfalexis@gmail.com>
2023-02-17 11:57:35 +0100

    start rectList #2

--- client/src/api/recep.api.tsx
+++ client/src/api/recep.api.tsx
@@ -2,13 +2,17 @@ import axios from 'axios'
 
 // ----------SHOULD ALL------------//
 
+const baseUserUrl = ('http://localhost:4000/api/v1/users')
+
+const baseRecipeUrl = ('localhost:4000/api/v1/recipes')
+
 export const getUsersRequest = async () => {
-   return axios.get('http://localhost:4000/api/v1/users')
+   return axios.get(baseUserUrl)
 
 }
 
 export const getRecipeRequest = async () => {
-   return axios.get('localhost:4000/api/v1/recipes')
+   return axios.get('http://localhost:4000/api/v1/recipes')
 
 }
 
--- client/src/pages/RectListPage.tsx
+++ client/src/pages/RectListPage.tsx
@@ -1,20 +1,44 @@
 import { type } from 'os'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getRecipeRequest } from '../api/recep.api';
 import Layout from '../components/Layout'
 
-type Props = {
+type Recipe = {
+  title:string;
+  description:string;
+  author: any;
+  ingredientes:Array<any>;
+  comment:string;
+
   
 }
 
 function RectListPage() {
+  const [recipes, setRecipes] = useState <Recipe[]> ([])
 
   useEffect(()=>{
+    async function loardRecipes () {
+      const response = await getRecipeRequest()
+      setRecipes(response.data)
+    }
+    loardRecipes()
 
-    console.log('RectListPage')
-  }, [])
+  }, 
+  [])
+
   return (
     <div>
-      <h1>RectList</h1>
+      <h1>RectList</h1>{
+        recipes.map(recipes=>(
+          <div>
+            <h1>{recipes.title}</h1>
+            <p>{recipes.description}</p>
+            <p>{recipes.author}</p>
+            <p>{recipes.ingredientes}</p>
+            <p>{recipes.comment}</p>
+          </div>
+        ))
+      }
       <Layout />
     </div>
   )
