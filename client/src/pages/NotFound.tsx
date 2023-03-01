60e878467ccb365bee054debc8a82cd648b1c798
mori <armigofire@gmail.com>
2023-03-01 20:23:44 +0100

    recette-list merge

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Layout from '../partials/Layout'
+import { Link } from 'react-router-dom';
+import './styles/NotFound.css';
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 - No Found</h1>
+    <div className="container">
+      <h1>Oops! You seem to be lost.</h1>
+      <p>Here are some helpful links:</p>      
+      <div>
+        <button className="button-primary">
+          <Link to='/'>Home</Link>
+        </button>        <button className="button-primary">
+          <Link to='/login'>Login</Link>
+        </button>
+      </div>
     </div>
   )
-}
-
-export default NotFound
+}export default NotFound
